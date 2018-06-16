e6ee7c4677697cbb482e1a0bea35d616dfd82461
s-t-h <si.ha@live.de>
2018-06-16 13:13:24 +0200

    [REWORK] Python3 and Comments

--- CI_SVM/Preprocessing.py
+++ CI_SVM/Preprocessing.py
@@ -1,88 +1,197 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.decomposition import TruncatedSVD
-from sklearn.model_selection import GridSearchCV
-from scipy import sparse
 import numpy as np
 
-#returns a 2d list with shape(n_peptides,9_aas)
-def parse(filename, labeled):
-	proteins=[]
-	if labeled:    	
-		labels = []
-#read the peptides from the file	
-	with open(filename,"r") as raw_data:
-		if labeled:		
-			raw_data.readline()
-    		for line in raw_data:
-			proteins.append(line.split()[0])
-			if labeled:        		
-				labels.append(line.split()[2])
-	if labeled:	
-		return proteins,labels
-	else:
-		return proteins
+'''
+TODO: Class Description
+'''
 
-#receives a 2d list of shape(n_peptides,9_aas) and returns the encoder together with an integer representation of the input
+
+def parse(file_path, labeled):
+
+    """
+    Returns a 2D list with shape(n_peptides,9_aas)
+
+    TODO: Tag description
+    :param file_path:
+    :param labeled:
+    :return:
+    """
+
+    global labels
+    proteins = []
+
+    if labeled:
+        labels = []
+
+    with open(file_path, "r") as raw_data:
+
+        if labeled:
+            raw_data.readline()
+
+        for line in raw_data:
+            proteins.append(line.split()[0])
+
+            if labeled:
+                labels.append(line.split()[2])
+
+    if labeled:
+        return proteins, labels
+
+    else:
+        return proteins
+
+
 def int_encode_train(proteins):
-#each amino acid is a new object in the list aa_s	
-	aa_s = []	
-	for protein in proteins:
-		for aa in protein:
-			aa_s.append(aa)
-#encode the amino acids with an integer	
-	int_encoder = LabelEncoder()
-	int_encoded = int_encoder.fit_transform(aa_s)
-	return int_encoder,int_encoded
 
-#receives a 2d list of shape(n_peptides,9_aas) and returns an integer representation of the input
-def int_encode_test(proteins,int_encoder):
-#each amino acid is a new object in the list aa_s	
-	aa_s = []	
-	for protein in proteins:
-		for aa in protein:
-			aa_s.append(aa)
-#encode the amino acids with an integer	
-	int_encoded = int_encoder.transform(aa_s)
-	return int_encoded
+    """
+    Receives a 2D list of shape(n_peptides,9_aas).
+    Method should be used for training a new model.
 
-#receives a 2d list of shape(n_proteins,9_aas) with int encoded proteins and returns the encoder together with the sparse encoded representation
+    TODO: Tag description
+    :param proteins:
+    :return:
+    """
+
+    # Each amino acid is a new object in the list aa_s
+    aa_s = []
+
+    for protein in proteins:
+        for aa in protein:
+            aa_s.append(aa)
+
+    # Fits a new LabelEncoder on aa's.
+    new_int_encoder = LabelEncoder()
+    # Encode the amino acids with an integer
+    aa_s_int_encoded = new_int_encoder.fit_transform(aa_s)
+
+    return new_int_encoder, aa_s_int_encoded
+
+
+def int_encode_predict(proteins, fitted_int_encoder):
+
+    """
+    Receives a 2D list of shape(n_peptides,9_aas) and returns an integer representation of the input.
+    Method should be used for predicting new data.
+
+    TODO: Tag description
+    :param proteins:
+    :param fitted_int_encoder:
+    :return:
+    """
+
+    # Each amino acid is a new object in the list aa_s
+    aa_s = []
+
+    for protein in proteins:
+        for aa in protein:
+            aa_s.append(aa)
+
+    # Encode the amino acids with an integer
+    aa_s_int_encoded = fitted_int_encoder.transform(aa_s)
+
+    return aa_s_int_encoded
+
+
 def sparse_encode_train(proteins):
-#one hot encode the amino acids
-	bin_encoder = OneHotEncoder()
-	bin_encoded = bin_encoder.fit_transform(proteins)
-    	return bin_encoder,bin_encoded
 
-#receives a 2d list of shape(n_proteins,9_aas) with int encoded proteins and returns the sparse encoded representation
-def sparse_encode_test(proteins,bin_encoder):
-#one hot encode the amino acids
-	bin_encoded = bin_encoder.transform(proteins)
-    	return bin_encoded
+    """
+    Receives a 2D list of shape(n_proteins,9_aas) with int encoded aa's and returns
+    a fitted OneHotEncoder together with the sparse encoded representation.
+    Method should be used for training a new model.
 
-#recover the encoded peptides
-def recover_from_sparse(bin_encoder,int_encoder,proteins_bin_encoded, n_proteins):
-	int_encoded = np.array([bin_encoder.active_features_[col] for col in proteins_bin_encoded.sorted_indices().indices]).reshape(n_proteins,9) - bin_encoder.feature_indices_[:-1]
-	recovered=int_encoder.inverse_transform([int_encoded])[0]
-    	return recovered
+    TODO: Tag description
+    :param
+    :return:
+    """
+
+    # Fits a new OneHotEncoder on int encoded proteins.
+    new_bin_encoder = OneHotEncoder()
+
+    # one-hot-encode the int encoded proteins.
+    proteins_bin_encoded = new_bin_encoder.fit_transform(proteins)
+
+    return new_bin_encoder, proteins_bin_encoded
 
 
+def sparse_encode_predict(proteins, fitted_bin_encoder):
 
-#extract sparse features from the raw data and return every encoding and processing model that was used
-def feature_extraction_sparse_train(filename):
-	features,labels = parse(filename,True)
-	n = len(features)
-	int_encoder,int_encoded = int_encode_train(features)
-	int_encoded = int_encoded.reshape(n,9)
-	sparse_encoder,features = sparse_encode_train(int_encoded)
-	return features,labels,sparse_encoder,int_encoder
+    """
+    Receives a 2D list of shape(n_proteins,9_aas) with int encoded proteins.
+    Returns the sparse encoded representation.
+    Method should be used for predicting new data.
 
-#extract sparse features from the raw data
-def feature_extraction_sparse_test(filename,sparse_encoder,int_encoder):
-	features = parse(filename,False)
-	n = len(features)
-	int_encoded = int_encode_test(features,int_encoder)
-	int_encoded = int_encoded.reshape(n,9)
-	features = sparse_encode_test(int_encoded,sparse_encoder)
-	return features
+    TODO: Tag description
+    :param
+    :param
+    :return:
+    """
 
-	
+    # one-hot-encode the proteins.
+    proteins_bin_encoded = fitted_bin_encoder.transform(proteins)
+
+    return proteins_bin_encoded
+
+
+def recover_from_sparse(bin_encoder, int_encoder, proteins_bin_encoded, n_proteins):
+
+    """
+    Recovers sparse encoded peptides.
+
+    TODO: Tag description
+    :param bin_encoder:
+    :param int_encoder:
+    :param proteins_bin_encoded:
+    :param n_proteins:
+    :return:
+    """
+
+    int_encoded = np.array(
+        [bin_encoder.active_features_[col] for col in proteins_bin_encoded.sorted_indices().indices]).reshape(
+        n_proteins, 9) - bin_encoder.feature_indices_[:-1]
+
+    recovered = int_encoder.inverse_transform([int_encoded])[0]
+
+    return recovered
+
+
+def feature_extraction_sparse_train(file_path):
+
+    """
+    Extract sparse features from the raw data and return every encoding and processing model that was used.
+    Method should be used for training a new model.
+
+    TODO: Tag description
+    :param file_path:
+    :return:
+    """
+
+    features, labels_ = parse(file_path, True)
+    n = len(features)
+    int_encoder, int_encoded = int_encode_train(features)
+    int_encoded = int_encoded.reshape(n, 9)
+    sparse_encoder, features = sparse_encode_train(int_encoded)
+
+    return features, labels_, sparse_encoder, int_encoder
+
+
+def feature_extraction_sparse_predict(file_path, sparse_encoder, int_encoder):
+
+    """
+    Extract sparse features from the raw data.
+    Method should be used for predicting new data.
+
+    TODO: Tag description
+    :param file_path:
+    :param sparse_encoder:
+    :param int_encoder:
+    :return:
+    """
+
+    features = parse(file_path, False)
+    n = len(features)
+    int_encoded = int_encode_predict(features, int_encoder)
+    int_encoded = int_encoded.reshape(n, 9)
+    features = sparse_encode_predict(int_encoded, sparse_encoder)
+
+    return features
--- CI_SVM/SVM_classifier.py
+++ CI_SVM/SVM_classifier.py
@@ -3,36 +3,80 @@ from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
-import numpy as np
 
-#fits an svm to a sparse representation of the features. The regularization constant C and the kernel are determined by grid search with 10 fold cross validation
-def train_sparse( filename ):
-    	svm = SVC()
-    	features,labels,sparse_encoder,int_encoder = Preprocessing.feature_extraction_sparse_train(filename)
-	param_grid={'kernel':['poly','linear','rbf'], 'C': [0.1,0.5,0.9,1,2]}
-	best_svm = GridSearchCV(svm, param_grid,cv=10)     	
-	best_svm.fit(features,labels)
-    	return best_svm,sparse_encoder,int_encoder
+'''
+TODO: Class Description
+'''
 
-def validate_model(testfile,svm,sparse_encoder,int_encoder):
-	features = Preprocessing.feature_extraction_sparse_test(testfile,sparse_encoder,int_encoder)
-	print "prediction: " + str(svm.predict(features))
 
-#traines an svm modell by applying dimensionality reduction to the features first. Hyperparameters, e.g. dimensionality of features, regularization constant C and the kernel being used are determined by Grid search, with 10-fold CV
-def train_dimension_reduction(filename):
-	pipe = Pipeline([('reduce_dim', TruncatedSVD()), ('classification', SVC())])
-	param_grid={'reduce_dim__n_components': [70,75,85,100,120], 'classification__kernel':['poly','linear','rbf'], 'classification__C':[0.1,0.5,1,2]}
-	svm = GridSearchCV(pipe, param_grid,cv=10)
-	features,labels,sparse_encoder,int_encoder = Preprocessing.feature_extraction_sparse_train(filename)
-	svm.fit(features,labels)
-	return svm,sparse_encoder,int_encoder
+def train_sparse(file_path):
+
+    """
+    Fits an svm to a sparse representation of the features.
+    The regularization constant C and the kernel are determined by grid search with 10 fold cross validation.
+
+    TODO: Tag description
+    :param file_path:
+    :return: best_svm:
+    """
+
+    new_svm = SVC()
+    features, labels, new_sparse_encoder, new_int_encoder = Preprocessing.feature_extraction_sparse_train(file_path)
+
+    param_grid = {'kernel': ['poly', 'linear', 'rbf'], 'C': [0.1, 0.5, 0.9, 1, 2]}
+    best_svm = GridSearchCV(new_svm, param_grid, cv=10)
+    best_svm.fit(features, labels)
+
+    return best_svm, new_sparse_encoder, new_int_encoder
+
+
+def train_dimension_reduction(file_path):
+
+    """
+    Trains an SVM by applying dimensionality reduction to the features first.
+    Hyperparameters, e.g. dimensionality of features, regularization constant C and
+    the kernel being used are determined by Grid search, with 10-fold CV.
+
+    :param file_path:
+    :return: best_svm:
+    """
+
+    pipe = Pipeline([('reduce_dim', TruncatedSVD()), ('classification', SVC())])
+    param_grid = {'reduce_dim__n_components': [70, 75, 85, 100, 120],
+                  'classification__kernel': ['poly', 'linear', 'rbf'], 'classification__C': [0.1, 0.5, 1, 2]}
+
+    best_svm = GridSearchCV(pipe, param_grid, cv=10)
+    features, labels, new_sparse_encoder, new_int_encoder = Preprocessing.feature_extraction_sparse_train(file_path)
+    best_svm.fit(features, labels)
+
+    return best_svm, new_sparse_encoder, new_int_encoder
+
+
+def validate_model(file_path, fitted_svm, fitted_sparse_encoder, fitted_int_encoder):
+    """
+    Validates a fitted model.
+
+    TODO: Tag description
+    :param file_path:
+    :param fitted_svm:
+    :param fitted_sparse_encoder:
+    :param fitted_int_encoder:
+    :return:
+    """
+
+    features = Preprocessing.feature_extraction_sparse_predict(file_path, fitted_sparse_encoder, fitted_int_encoder)
+
+    print("prediction: " + str(fitted_svm.predict(features)))
+
 
 if __name__ == "__main__":
-	#svm,sparse_encoder,int_encoder = train_dimension_reduction("project_training.txt")
-	svm, sparse_encoder,int_encoder = train_sparse("project_training.txt")	
-	print "best params"	
-	print svm.best_params_
-	print "best score"
-	print svm.best_score_
-	validate_model("test_input.txt",svm,sparse_encoder,int_encoder)
-	
+
+    # svm,sparse_encoder,int_encoder = train_dimension_reduction("project_training.txt")
+    svm, sparse_encoder, int_encoder = train_sparse("project_training.txt")
+
+    print("best params")
+    print(svm.best_params_)
+    print("best score")
+    print(svm.best_score_)
+
+    validate_model("test_input.txt", svm, sparse_encoder, int_encoder)
