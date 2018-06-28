c19e512f4b39f799478e9ffcc40857fe0c4e1f2f
maffos <matthias_wamhoff@web.de>
2018-06-28 23:05:12 +0200

    new file structure and cross validation for hmm

--- HMM/Main.py
+++ HMM/Main.py
@@ -2,18 +2,22 @@ from pomegranate import *
 from InitMatrices import InitMatrices
 from Data import Data
 from HMMConstruct import HMMConstruct
+#from Validation import kfold_cross_validation
 
 
 class HMM():
 
-    if __name__ == '__main__':
+    def __init__(self, pos_path, neg_path, test_path, output_path):
+        self.pos_path = pos_path
+        self.neg_path = neg_path
+        self.test_path = test_path
+        self.output_path = output_path
+        self.model_pos = None
+        self.model_neg = None
 
-        pos_path = 'D:\Dropbox\Working directory\#Material\data_positive.txt'
-        neg_path = 'D:\Dropbox\Working directory\#Material\data_negative.txt'
-        test_path = 'D:\Dropbox\Working directory\#Material\\kaggle.txt'
-        output_path = 'D:\Dropbox\Working directory\#Material\output.txt'
+    def train(self):
 
-        init_data = Data(pos_path, neg_path)
+        init_data = Data(self.pos_path, self.neg_path)
         positive_pairs, negative_pairs = init_data.split_in_pairs()
 
         init_pos = InitMatrices(positive_pairs)
@@ -24,43 +28,55 @@ class HMM():
         trans_matrix_neg = init_neg.compute_transitions()
         emission_matrix_neg = init_neg.compute_emissions()
 
-        model_pos = HMMConstruct(trans_matrix_pos, emission_matrix_pos,
-                                 pos_path)
-        model_pos.build_eval_HMM()
+        self.model_pos = HMMConstruct(trans_matrix_pos, emission_matrix_pos,
+                                 self.pos_path)
+        self.model_pos.build_eval_HMM()
 
-        model_neg = HMMConstruct(trans_matrix_neg, emission_matrix_neg,
-                                 neg_path)
-        model_neg.build_eval_HMM()
+        self.model_neg = HMMConstruct(trans_matrix_neg, emission_matrix_neg,
+                                 self.neg_path)
+        self.model_neg.build_eval_HMM()
 
-        text_file = open(output_path, "w")
+    def predict(self):
+        text_file = open(self.output_path, "w")
         text_file.write("Id,Prediction1\n")
-
+        prediction = []
         # Extract only the sequences from our file.
-        with open(test_path, 'r') as tf:
+        with open(self.test_path, 'r') as tf:
             data = [line[:9] for line in tf]
 
         # For each sequence in data calculate the negative log likelihood of it
         # being a binder (positive model) or a non-binder (negative model).
         for line in data:
             test_data = []
-
             # Split current sequence in single characters.
             for i in range(len(line)):
                 test_data.append(line[i])
 
             # Viterbi-Algorithm calculates the negative log likelihood of each
             # sequence.
-            neg_log_likelihood_1 = model_pos.log_probability(test_data)
-            neg_log_likelihood_2 = model_neg.log_probability(test_data)
+            neg_log_likelihood_1 = self.model_pos.log_probability(test_data)
+            neg_log_likelihood_2 = self.model_neg.log_probability(test_data)
 
             # Decides which outcome is more likely and writes it to a file at
             # given path.
             if (neg_log_likelihood_1 >= neg_log_likelihood_2):
                 text_file.write(line + ',1\n')
+                prediction.append(1)
                 # print(line + ',1')
             else:
                 text_file.write(line + ',0\n')
+                prediction.append(0)
                 # print(line + ',0')
 
         text_file.close()
         print('Process successful.')
+        return prediction
+
+if __name__ == "__main__":
+    pos_path = "data_positive.txt"
+    neg_path = "data_negative.txt"
+    test_path = "kaggle.txt"
+    output_path = 'output.txt'
+    hmm = HMM(pos_path, neg_path, test_path, output_path)
+    hmm.train()
+    prediction = hmm.predict()
--- old/SVM_classifier.py
+++ old/SVM_classifier.py
@@ -1,26 +1,28 @@
 import Preprocessing
 import Encodings
 from sklearn.svm import SVC
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import TruncatedSVD,PCA
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 import numpy as np
+from matplotlib import pyplot as plt
 
 #fits an svm to a sparse representation of the features. The regularization constant C and the kernel are determined by grid search with 10 fold cross validation
 def train_sparse( filename ):
-    	svm = SVC()
+    	svm = SVC(kernel='linear')
     	features,labels,sparse_encoder,int_encoder = Preprocessing.feature_extraction_sparse_train(filename)
-	param_grid={'kernel':['rbf','linear','poly'], 'degree': [2,3,4], 'C': [0.1,0.5,0.9,1,2]}
+	param_grid={'C': [0.1,0.5,0.9,1,2]}
 	best_svm = GridSearchCV(svm, param_grid,cv=10)     	
 	best_svm.fit(features,labels)
     	return best_svm,sparse_encoder,int_encoder
 
 #traines an svm modell by applying dimensionality reduction to the features first. Hyperparameters, e.g. dimensionality of features, regularization constant C and the kernel being used are determined by Grid search, with 10-fold CV
 def train_dimension_reduction(filename):
-	pipe = Pipeline([('reduce_dim', TruncatedSVD()), ('classification', SVC())])
-	param_grid={'reduce_dim__n_components': [70,75,85,100,120], 'classification__kernel':['rbf','linear','poly'], 'classification__degree': [2,3,4],'classification__C':[0.5,0.9,1,2]}
+	pipe = Pipeline([('reduce_dim', PCA(n_components=75)), ('classification', SVC())])
+	param_grid={'classification__kernel':['rbf','linear','poly'], 'classification__degree': [2,3,4],'classification__C':[0.5,0.9,1,2]}
 	svm = GridSearchCV(pipe, param_grid,cv=10)
 	features,labels,sparse_encoder,int_encoder = Preprocessing.feature_extraction_sparse_train(filename)
+	features = features.toarray()
 	svm.fit(features,labels)
 	return svm,sparse_encoder,int_encoder
 
@@ -41,8 +43,8 @@ def train_categorical_numerical( filename ):
 	return best_svm,onehot_encoder,int_encoder
 
 def train_categorical_numerical_dimension_reduction(filename):
-	pipe = Pipeline([('reduce_dim', TruncatedSVD()), ('classification', SVC())])
-	param_grid={'reduce_dim__n_components': [70,85,100,120,140,140], 'classification__C':[0.1,0.5,1,2], 'classification__kernel':['linear','poly','rbf'], 'classification__degree':[2,3,4]}
+	pipe = Pipeline([('reduce_dim', TruncatedSVD(n_components=75)), ('classification', SVC(kernel='linear'))])
+	param_grid={'classification__C':[0.1,0.4,0.9,1,2]}
 	svm = GridSearchCV(pipe, param_grid,cv=10)
 	features,labels,onehot_encoder,int_encoder = Preprocessing.feature_extraction_categorical_numerical_train(filename)
 	svm.fit(features,labels)
@@ -50,6 +52,7 @@ def train_categorical_numerical_dimension_reduction(filename):
 
 def validate_model(testfile,svm,sparse_encoder,int_encoder):
 	features = Preprocessing.feature_extraction_sparse_test(testfile,sparse_encoder,int_encoder)
+	features = features.toarray()	
 	prediction = svm.predict(features)
 	#print "prediction: " + str(svm.predict(features))
 	with open("prediction.csv", "w") as out:
@@ -74,7 +77,24 @@ def validate_physicochemical(testfile, svm, onehot_encoder):
 			with open("prediction.csv", "a") as out:
 				out.write(str(peptide) + "," + str(prediction[index]+"\n"))
 
-
+def dimension_reduction():
+	features, labels,sparse_encoder,int_encoder = Preprocessing.feature_extraction_sparse_train("project_training.txt")
+	X = [179,170,160,150,140,130,120,110,100,90,80,70,60,50]
+	Y = []
+	for x in X:
+		svd = TruncatedSVD(n_components = x)
+		svd.fit(features)
+		Y.append(1-sum(svd.explained_variance_ratio_))
+	
+	for index,x in enumerate(X):
+		X[index] = 180-x
+	plt.xlabel("Number of reduced components")
+	plt.ylabel("Percentage of Information lost")
+	plt.title("Information loss from dimensionality reduction")
+	plt.plot(X,Y)
+	plt.grid()
+	plt.show()
+	
 def validate_categorical_numerical(testfile, svm, onehot_encoder, int_encoder):
 	features = Preprocessing.feature_extraction_categorical_numerical_test(testfile, onehot_encoder,int_encoder)
 	prediction = svm.predict(features)
@@ -94,6 +114,7 @@ if __name__ == "__main__":
 	#svm,onehot_encoder = train_physicochemical("project_training.txt")	
 	#svm,onehot_encoder,int_encoder = train_categorical_numerical_dimension_reduction("project_training.txt")
 	#svm, onehot_encoder, int_encoder = train_categorical_numerical("project_training.txt")
+	#dimension_reduction()
 	print "best params"	
 	print svm.best_params_
 	print "best score"
